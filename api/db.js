ed34b625774bcafd202789c40cda45263dc66159
Juhasz, Balazs <balazs.juhasz@siemens-healthineers.com>
2018-06-18 22:14:39 +0200

    added upload function and request of items' count

--- api/db.js
+++ api/db.js
@@ -2,21 +2,29 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-let database = process.env.DATABASE || "@localhost:27017";
-const url = `mongodb://${database}`
+const localDB = "@localhost:27017";
+let database = process.env.DATABASE || localDB;
+
+function URL(db) { return `mongodb://${db}`; }
 
 // Database Name
 const dbName = 'helloworld-database';
+const RSFA_COLLECTION = "rsfa";
 
 async function Test() {
+    const url = URL(database);
     console.log(`connecting to ${url}`);
     client = await MongoClient.connect(url);
     console.log("Connected successfully to server");
 
     const db = client.db(dbName);
+    const rsfa = db.collection(RSFA_COLLECTION);
+    const rsfaCount = await (rsfa.find().count());
+    console.log(`rsfaCount: ${rsfaCount}`);
     const result = {
         query_time: (new Date()).toString(),
-        collections: (await db.listCollections().toArray()).map(x => x.name)
+        collections: (await db.listCollections().toArray()).map(x => x.name),
+        size_of_rsfa: rsfaCount
     };
 
     client.close();
@@ -25,4 +33,51 @@ async function Test() {
     return result;
 }
 
-module.exports = { Test };
+async function Upload() {
+    if (database !== localDB) {
+        return { error: "Upload is not possible due to the current environment. (No connection specified to a cloud DB.)" };
+    }
+    let clients = [];
+    const getCollection = async (dbPath, title) => {
+        try {
+            const url = URL(dbPath);
+            console.log(`connecting to ${url}`);
+            client = await MongoClient.connect(url);
+            clients.push(client);
+            console.log(`Connected successfully to ${title} server`);
+            const db = client.db(dbName);
+            return db.collection(RSFA_COLLECTION);
+        }
+        catch (err) {
+            console.log(err);
+            return null;
+        }
+    }
+
+    let [local, cloud] = [null, null];
+    try {
+        [local, cloud] = await Promise.all([
+            getCollection(localDB, 'local'),
+            getCollection(process.env.CLOUD_DB, 'cloud')
+        ]);
+        if (!local || !cloud) throw "Upload aborted due to database connection error";
+
+        var bulk = cloud.initializeUnorderedBulkOp();
+        const localItems = await local.find().toArray();
+        for (let i = 0; i < localItems.length; i++) {
+            bulk.insert(localItems[i]);
+        }
+        bulk.execute();
+
+        return { success: "Upload finished successfully" };
+    }
+    catch (err) {
+        console.log(err);
+        return { error: err };
+    }
+    finally {
+        clients.forEach(c => c.close());
+    }
+}
+
+module.exports = { Test, Upload };
--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const { Test } = require('./api/db');
+const { Test, Upload } = require('./api/db');
 
 app.prepare().then(() => {
     const server = express();
 
     server.get('/api/db', async (req, res) => {
         res.send(await Test());
+    });
+
+    server.get('/api/upload', async (req, res) => {
+        res.send(await Upload());
     });
 
     server.get('*', (req, res) => {
